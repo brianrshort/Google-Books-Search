c86cb3ce25a75bd749bca3e3ff3d32c5b7b95993
Brian Short <brian.ralph.short@gmail.com>
2021-02-07 23:03:44 -0500

    Add main saved searches page to display books from database

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+export function Saved() {
+    const [display, setDisplay] = useState([]);
+
+    useEffect(() => {
+        axios.get("/api/books").then(res => {
+            console.log(res);
+            setDisplay(res);
+            console.log(display);
+            //console.log(res.data.items);
+        }).catch(err => {
+            console.log(err)
+        })
+    }, [display]);
+
+    return (
+        <div>
+        {display.map(volume => {
+            return (
+                <div className="m-3 col-12 container card">
+                    <div className="card-body">
+                    <img className="mx-3" style={{ float: "left"}} src={volume.image} alt="Book cover" />
+                    <a href={volume.link}>
+                    <h4>{volume.title}</h4>
+                    </a>
+                    <span>Authors -- </span>
+                    {volume.authors.map(auth => {
+                        return (
+                <span>
+                    <span>{auth}</span><span> // </span>
+                    </span>
+            )})}
+            <p>{volume.description}</p>
+            </div>
+            </div>
+            )
+        })}
+        </div>
+    )
+
+}
+
+
