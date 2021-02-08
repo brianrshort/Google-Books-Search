be3af361f1c18338d683e925f1021cbeacf7e57f
Brian Short <brian.ralph.short@gmail.com>
2021-02-07 23:00:10 -0500

    Add footer component to display on multiple pages

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Footer() {
+    return (
+        <footer className="footer mb-0">
+            <div className="container">
+                <span className="text-muted">Footer Construction Co. (c) 2021</span>
+            </div>
+        </footer>
+    )
+}
