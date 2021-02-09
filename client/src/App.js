bd01fad3d8387bf88e3a547023ea174005d062d4
Brian Short <brian.ralph.short@gmail.com>
2021-02-08 21:33:13 -0500

    Add link buttons to the home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import { Jumbotron } from "./components/Jumbotron";
-import { Footer } from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { Jumbotron } from './components/Jumbotron';
+import { Footer } from './components/Footer';
 import { Search } from './pages/Search';
 import { Saved } from './pages/Saved';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
       <Navbar />
       <Jumbotron />
       <Switch>
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/saved" component={Saved} /> 
+        <Route exact path='/' component={Home} />
+        <Route exact path='/search' component={Search} />
+        <Route exact path='/saved' component={Saved} /> 
       </Switch>
       <Footer />
       </Router>
